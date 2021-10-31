5e0116a9a5d11c4e77b18d3e6a9add9c8c85fc09
Ryuto Setsujin <32800772+RyutoSetsujin@users.noreply.github.com>
2021-10-30 21:04:11 -0600

    Add files via upload

--- BGAnimations/ScreenSelectMusic underlay/default.lua
+++ BGAnimations/ScreenSelectMusic underlay/default.lua
@@ -3,24 +3,18 @@ local t = Def.ActorFrame {};
 if not GAMESTATE:IsCourseMode() then
 	local function JacketUpdate(self)
 		local song = GAMESTATE:GetCurrentSong();
-		local Jacket = self:GetChild("Jacket");
-		local height = Jacket:GetHeight();
+		local Background = self:GetChild("Background");
+		local height = Background:GetHeight();
 		
 		if song then
-			if song:HasBackground() then
-				Jacket:visible(true);
-				Jacket:Load(song:GetBackgroundPath());
-				Jacket:scaletoclipped(854,480);
-				Jacket:fadeleft(0.25);
-				Jacket:faderight(0.25);
-			elseif song:HasJacket() then
-				Jacket:visible(true);
-				Jacket:Load(song:GetJacketPath());
-				Jacket:scaletoclipped(1200,1200);
-			elseif song:HasBanner() then
-				Jacket:visible(true);
-				Jacket:Load(song:GetBannerPath());
-				Jacket:scaletoclipped(1880,720);
+			if song:HasJacket() then
+				Background:visible(true);
+				Background:Load(song:GetBackgroundPath());
+				Background:scaletoclipped(1920,1080);
+			else
+				Background:visible(true);
+				Background:Load(song:GetBannerPath());
+				Background:scaletoclipped(1580,720);
 			end;
 		else
 			Jacket:visible(false);
@@ -28,49 +22,12 @@ if not GAMESTATE:IsCourseMode() then
 		
 	end;
 	t[#t+1] = Def.ActorFrame {
-		InitCommand=cmd(x,SCREEN_CENTER_X+50;y,SCREEN_CENTER_Y;diffusealpha,0.75;SetUpdateFunction,JacketUpdate);
-		OnCommand=cmd(x,SCREEN_CENTER_X+50;sleep,3;linear,0.25;diffusealpha,0.75;accelerate,3.25;x,SCREEN_CENTER_X-50;diffusealpha,0;queuecommand,"Right");
-		RightCommand=cmd(x,SCREEN_CENTER_X-50;sleep,3;linear,0.25;diffusealpha,0.75;accelerate,3.25;x,SCREEN_CENTER_X+50;diffusealpha,0;queuecommand,"On");
-		OffCommand=cmd(diffusealpha,0);
+		InitCommand=cmd(x,SCREEN_CENTER_X+100;y,SCREEN_CENTER_Y;diffusealpha,0.25;SetUpdateFunction,JacketUpdate);
+		OnCommand=cmd(x,SCREEN_CENTER_X+100;sleep,3;linear,0.25;diffusealpha,0.25;accelerate,3.25;x,SCREEN_CENTER_X-100;sleep,2;accelerate,3.25;x,SCREEN_CENTER_X+100;queuecommand,"On");
 		Def.Sprite {
-			Name="Jacket";
+			Name="Background";
 		};	
 	};
 end;
-
---Jacket and Banner overlay
-if not GAMESTATE:IsCourseMode() then
-	local function JacketUpdate(self)
-		local song = GAMESTATE:GetCurrentSong();
-		local Jacket = self:GetChild("Jacket");
-		local height = Jacket:GetHeight();
-		
-		if song then
-			if song:HasBackground() then
-				Jacket:visible(false);
-			elseif song:HasJacket() then
-				Jacket:visible(true);
-				Jacket:Load(song:GetJacketPath());
-				Jacket:scaletoclipped(300,300);
-			elseif song:HasBanner() then
-				Jacket:visible(true);
-				Jacket:Load(song:GetBannerPath());
-				Jacket:scaletoclipped(488,172);
-			end;
-		else
-			Jacket:visible(false);
-		end;
-		
-	end;
-	t[#t+1] = Def.ActorFrame {
-		InitCommand=cmd(x,SCREEN_CENTER_X+50;y,SCREEN_CENTER_Y;SetUpdateFunction,JacketUpdate);
-		OnCommand=cmd(x,SCREEN_CENTER_X+50;sleep,3;linear,0.25;diffusealpha,1;accelerate,3.25;x,SCREEN_CENTER_X-50;diffusealpha,0;queuecommand,"Right");
-		RightCommand=cmd(x,SCREEN_CENTER_X-50;sleep,3;linear,0.25;diffusealpha,1;accelerate,3.25;x,SCREEN_CENTER_X+50;diffusealpha,0;queuecommand,"On");
-		OffCommand=cmd(diffusealpha,0);
-		Def.Sprite {
-			Name="Jacket";
-		};	
-	};
-end;--]]
 
 return t
